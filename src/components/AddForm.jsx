3c34bb0d5439be99aed8b7908c8ab60898525e6f
ItsAlexFS <alexfitzsolis123@gmail.com>
2026-03-28 23:00:01 -0600

    Initial commit

---
+++ src/components/AddForm.jsx
@@ -0,0 +1,54 @@
+import React, {useState, useContext} from 'react'
+import {GlobalContext} from '../context/GlobalState'
+
+const AddForm = () => {
+
+    const [descripcion, setDescripcion] = useState("")
+    const [importe, setImporte] = useState(0)
+
+    const {addMovimiento} = useContext(GlobalContext)
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        const nuevoMovimiento = {
+            descripcion,
+            importe: +importe
+        }
+
+        addMovimiento(nuevoMovimiento)
+        setDescripcion('')
+        setImporte(0)
+    }
+
+  return (
+    <>
+        <h3>Agregar Movimiento</h3>
+        <form onSubmit={onSubmit}>
+            <div className="form-control">
+                <label htmlFor="descripcion">Descripción:</label>
+                <input 
+                    type="text" 
+                    id='descripcion' 
+                    value={descripcion} 
+                    placeholder='Teclea la descripción'
+                    onChange={(e) => setDescripcion(e.target.value)}
+                />
+            </div>
+            <div className="form-control">
+                <label htmlFor="importe">Importe:</label>
+                <input 
+                    type="number" 
+                    id='importe' 
+                    value={importe} 
+                    placeholder='Teclea el importe'
+                    onChange={(e) => setImporte(e.target.value)}
+                />
+            </div>
+            <button className='btn'>Agregar Movimiento</button>
+        </form>
+    </>
+  )
+}
+
+export default AddForm
---
+++ src/components/Balance.jsx
@@ -0,0 +1,19 @@
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
+const Balance = () => {
+
+    const {movimientos} = useContext(GlobalContext)
+
+    const importes = movimientos.map(movimiento => movimiento.importe)
+    const total = importes.reduce((acc, item) => (acc += item), 0).toFixed(2)
+
+  return (
+    <>
+        <h4>Tu Saldo</h4>
+        <h1>${total}</h1>
+    </>
+  )
+}
+
+export default Balance
